a1ec168d7ef3815039ef7e9b162388e33b2ed062
Showkat Akbar mithu <showkat131080@gmail.com>
2024-04-23 23:32:06 +0600

    Add project files.

---
+++ EninasHotel.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EninasHotel.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
