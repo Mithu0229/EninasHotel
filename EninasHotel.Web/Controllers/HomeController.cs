95fb367364b166b5418b1378a7744c45a746ea4c
Showkat Akbar mithu <showkat131080@gmail.com>
2024-04-29 21:22:58 +0600

    home page design

--- EninasHotel.Web/Controllers/HomeController.cs
+++ EninasHotel.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using EninasHotel.Application.Common.Interfaces;
 using EninasHotel.Web.Models;
+using EninasHotel.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +8,22 @@ namespace EninasHotel.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity"),
+                Nights = 1,
+                CheckInDate = DateOnly.FromDateTime(DateTime.Now),
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
---
+++ EninasHotel.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using EninasHotel.Domain.Entities;
+
+namespace EninasHotel.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
