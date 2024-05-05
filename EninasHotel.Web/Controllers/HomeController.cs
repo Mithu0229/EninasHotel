c99e7591f5a58ef06dac7d8449901609d290aa30
Showkat Akbar mithu <showkat131080@gmail.com>
2024-05-05 21:27:10 +0600

    jquery-ajax-unobtrusive using in home page and add this package jsdeliver by server site js file

--- EninasHotel.Web/Controllers/HomeController.cs
+++ EninasHotel.Web/Controllers/HomeController.cs
@@ -26,7 +26,43 @@ namespace EninasHotel.Web.Controllers
             return View(homeVM);
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Index(HomeVM homeVM)
+        {
+            homeVM.VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
+            foreach (var villa in homeVM.VillaList)
+            {
+                if (villa.Id % 2 == 0)
+                {
+                    villa.IsAvailable = false;
+                }
+            }
+            return View(homeVM);
+        }
+
+        public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
+        {
+            var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
+            foreach (var villa in villaList)
+            {
+                if (villa.Id % 2 == 0)
+                {
+                    villa.IsAvailable = false;
+                }
+            }
+            
+            HomeVM homeVM = new()
+            {
+                CheckInDate = checkInDate,
+                VillaList = villaList,
+                Nights = nights
+            };
+
+            return PartialView("_VillaList", homeVM);
+        }
+
+
+    public IActionResult Privacy()
         {
             return View();
         }
